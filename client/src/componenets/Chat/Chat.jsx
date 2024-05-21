4c5c48eea08f25b61acfab5cf728ef5a15e067f4
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-05-21 15:53:40 -0300

    Iniciando front

---
+++ client/src/componenets/Chat/Chat.jsx
@@ -0,0 +1,11 @@
+
+export default function Chat() {
+  return (
+    <div>
+        <h1> Chat </h1>
+        <input type="text" placeholder="Mensagem"/>
+        <button> Enviar </button>
+
+    </div>
+  )
+}
