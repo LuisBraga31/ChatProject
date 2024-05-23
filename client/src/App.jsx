1eb76ab2a4194dd7ac5838d42d73602fe1b5f398
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-05-23 14:53:26 -0300

    Criando logica para trocar entre 'Join' e 'Chat'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 
 import Join from './componenets/Join/Join'
 import Chat from './componenets/Chat/Chat'
+import { useState } from 'react'
 
 function App() {
 
+  const [chatVisibility, setChatVisibility] = useState(false);
 
   return (
-    <div>
-      <Join/>
-      <Chat/>
-
+    <div className="App">
+      {
+        chatVisibility ? <Chat/> : <Join setChatVisibility={setChatVisibility}/>
+      }
     </div>
   )
 }
--- client/src/componenets/Join/Join.jsx
+++ client/src/componenets/Join/Join.jsx
@@ -1,10 +1,22 @@
+/* eslint-disable react/prop-types */
+import { useRef } from "react"
 
-export default function Join() {
+export default function Join({setChatVisibility}) {
+
+  const usernameRef = useRef();
+  
+  const handleSubmit = () =>{
+    const username = usernameRef.current.value;
+    if(!username.trim()) return
+    setChatVisibility(true)
+  
+  }
+
   return (
     <div>
         <h1> Join </h1>
-        <input type="text" placeholder="Username"/>
-        <button> Entrar </button>
+        <input type="text" ref={usernameRef} placeholder="Username"/>
+        <button onClick={()=> handleSubmit()}> Entrar </button>
     </div>
   )
 }
