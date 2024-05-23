30aa53d1e85fb3d2afd718ea70eaa79430001e29
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-05-23 15:03:03 -0300

    Estabelecendo conexão entre client e server

--- client/src/componenets/Join/Join.jsx
+++ client/src/componenets/Join/Join.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/prop-types */
 import { useRef } from "react"
+import io from 'socket.io-client'
 
 export default function Join({setChatVisibility}) {
 
   const usernameRef = useRef();
   
-  const handleSubmit = () =>{
+  const handleSubmit = async () =>{
     const username = usernameRef.current.value;
     if(!username.trim()) return
+    const socket = await io.connect('http://localhost:3001')
+    socket.emit('set_username', username)
     setChatVisibility(true)
   
   }
